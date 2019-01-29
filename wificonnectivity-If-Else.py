fcb3172e22ff075ce665d03efedebd53d7d8b217
prgs28 <siddhati28@gmail.com>
2019-01-28 18:34:19 -0600

    Add files via upload

---
+++ wificonnectivity-If-Else.py
@@ -0,0 +1,17 @@
+
+
+print("WI-FI not working?!\n Reboot the system and try to connect")
+answer=input("did that fix the problem(y/n)?")
+if(answer=='n' or answer=='N'):
+      print("Reboot the system and try to connect")
+      answer=input("did that fix the problem(y/n)?")
+      if(answer=='n' or answer=='N'):
+          print("make sure the cabels between router and modem are plugged in firmly")
+          answer=input("did that fix the problem(y/n)?")
+          if(answer=='n' or answer=='N'):
+              print("move the router to a new location")
+              answer=input("did that fix the problem(y/n)?")
+              if(answer=='n' or answer=='N'):
+                  print("get a new router")
+
+ 
